bc93ffa672ee0fd8160050ae7b2ed92f89955f13
zhangwenxiang <562162944@qq.com>
2024-12-21 18:54:13 +0800

    dm抽象类实现

--- src/main/java/top/zwx/eazyDB/common/Error.java
+++ src/main/java/top/zwx/eazyDB/common/Error.java
@@ -5,7 +5,7 @@ public class Error {
     public static final Exception FileExistsException = new RuntimeException("File already exists!");
     public static final Exception FileCannotRWException = new RuntimeException("File cannot read or write!");
     public static final Exception FileNotExistsException = new RuntimeException("File does not exists");
-
+    public static final Exception CacheFullException = new RuntimeException("Cache is full!");
     // tm
     public static final Exception BadXIDFileException = new RuntimeException("Bad XID file!");
 }
